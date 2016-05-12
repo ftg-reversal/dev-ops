4b9e59c6c91a60a9f288888d0f1f6f132a71068b
tigberd <tigberd@gmail.com>
2016-05-13 04:41:30 +0900

    appサーバのamiをpackerで作ったものに修正

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "reversal-webserver" {
-    ami                         = "ami-d8cf28b9"
+    ami                         = "ami-df7790be"
     availability_zone           = "ap-northeast-1c"
     instance_type               = "t2.micro"
     key_name                    = "reversal"
