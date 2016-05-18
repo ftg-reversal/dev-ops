c5b8826b02c25e6853660b83d253edcf3f310b24
yuki_yano <yuki_yano@dwango.co.jp>
2016-05-18 17:43:39 +0900

    amiを更新

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "reversal-webserver" {
-    ami                         = "ami-bd19f8dc"
+    ami                         = "ami-a91afbc8"
     availability_zone           = "ap-northeast-1c"
     instance_type               = "t2.micro"
     key_name                    = "reversal"
