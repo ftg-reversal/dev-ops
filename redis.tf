e2087301b3a0fe89bc00599d69b45e36537d19a9
tigberd <tigberd@gmail.com>
2016-07-05 09:17:22 +0900

    Redisを作成

---
+++ redis.tf
@@ -0,0 +1,9 @@
+resource "aws_elasticache_cluster" "reversal_redis" {
+    cluster_id = "reversal"
+    engine = "redis"
+    node_type = "cache.m1.small"
+    port = 6379
+    num_cache_nodes = 1
+    parameter_group_name = "default.redis1.4"
+    subnet_group_name = "${aws_elasticache_subnet_group.reversal_redis_subnet.id}"
+}
--- vpc.tf
+++ vpc.tf
@@ -26,10 +26,23 @@ resource "aws_subnet" "reversal_private_db2" {
     map_public_ip_on_launch = false
 }
 
+resource "aws_subnet" "reversal_private_redis" {
+    vpc_id                  = "${aws_vpc.reversal_vpc.id}"
+    cidr_block              = "10.0.4.0/24"
+    availability_zone       = "ap-northeast-1a"
+    map_public_ip_on_launch = false
+}
+
 resource "aws_db_subnet_group" "reversal_db_subnet" {
     name        = "ftg-reversal"
     description = "Ftg-Reversal DB"
     subnet_ids  = ["${aws_subnet.reversal_private_db1.id}", "${aws_subnet.reversal_private_db2.id}"]
+}
+
+resource "aws_elasticache_subnet_group" "reversal_redis_subnet" {
+    name        = "ftg-reversal"
+    description = "Ftg-Reversal Redis"
+    subnet_ids  = ["${aws_subnet.reversal_private_redis.id}"]
 }
 
 resource "aws_internet_gateway" "public_gateway" {
