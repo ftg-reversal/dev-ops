9b25f42b7127fe882fad95ff2986a27528a00b80
tigberd <tigberd@gmail.com>
2016-07-21 01:52:51 +0900

    セキュリティグループの指定ミスを修正

--- ec2.tf
+++ ec2.tf
@@ -89,7 +89,8 @@ resource "aws_security_group" "store_security_group" {
         from_port       = 6379
         to_port         = 6379
         protocol        = "tcp"
-        cidr_blocks     = ["${aws_security_group.webserver_security_group.id}"]
+        security_groups = ["${aws_security_group.webserver_security_group.id}"]
+        self            = false
     }
 
     ingress {
@@ -116,7 +117,8 @@ resource "aws_security_group" "imperial_security_group" {
         from_port       = 80
         to_port         = 80
         protocol        = "tcp"
-        cidr_blocks     = ["${aws_security_group.webserver_security_group.id}"]
+        security_groups = ["${aws_security_group.webserver_security_group.id}"]
+        self            = false
     }
 
     ingress {
