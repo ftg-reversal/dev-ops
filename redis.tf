a8fd8342600679afa5c57b81295dacd2d7d313c5
tigberd <tigberd@gmail.com>
2016-07-21 00:55:33 +0900

    redisを一旦削除

--- redis.tf
+++ redis.tf
@@ -1,9 +1,9 @@
-resource "aws_elasticache_cluster" "reversal_redis" {
-    cluster_id = "reversal"
-    engine = "redis"
-    node_type = "cache.t2.micro"
-    port = 6379
-    num_cache_nodes = 1
-    parameter_group_name = "default.redis2.8"
-    subnet_group_name = "${aws_elasticache_subnet_group.reversal_redis1_subnet.id}"
-}
+/* resource "aws_elasticache_cluster" "reversal_redis" { */
+/*     cluster_id = "reversal" */
+/*     engine = "redis" */
+/*     node_type = "cache.t2.micro" */
+/*     port = 6379 */
+/*     num_cache_nodes = 1 */
+/*     parameter_group_name = "default.redis2.8" */
+/*     subnet_group_name = "${aws_elasticache_subnet_group.reversal_redis1_subnet.id}" */
+/* } */
