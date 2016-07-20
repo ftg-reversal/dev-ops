5622fb54c3ff28cff76f434a7efa613634732123
tigberd <tigberd@gmail.com>
2016-07-20 21:44:58 +0900

    パラメータのミスを修正

--- redis.tf
+++ redis.tf
@@ -4,6 +4,6 @@ resource "aws_elasticache_cluster" "reversal_redis" {
     node_type = "cache.m1.small"
     port = 6379
     num_cache_nodes = 1
-    parameter_group_name = "default.redis1.4"
+    parameter_group_name = "default.redis2.8"
     subnet_group_name = "${aws_elasticache_subnet_group.reversal_redis_subnet.id}"
 }
