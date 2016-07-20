dfe66b12c0a3c45784ff0555f922a5587ae4f5b7
tigberd <tigberd@gmail.com>
2016-07-20 23:18:06 +0900

    インスタンスタイプを変更

--- redis.tf
+++ redis.tf
@@ -1,7 +1,7 @@
 resource "aws_elasticache_cluster" "reversal_redis" {
     cluster_id = "reversal"
     engine = "redis"
-    node_type = "cache.m1.small"
+    node_type = "cache.m3.medium"
     port = 6379
     num_cache_nodes = 1
     parameter_group_name = "default.redis2.8"
