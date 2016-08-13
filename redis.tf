5b5d921a728ade8b1c9928de94c37a820d1c40aa
tigberd <tigberd@gmail.com>
2016-08-13 18:11:36 +0900

    Revert "Redisを試しに追加"
    This reverts commit 10a9cccd4a6e0abb4ed1d0365ce2f36b675616c7.

--- redis.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_elasticache_cluster" "redis" {
-    cluster_id = "reversal-redis"
-    engine = "redis"
-    port = 6379
-    node_type = "cache.t2.micro"
-    num_cache_nodes = 1
-    parameter_group_name = "default.redis2.8"
-    subnet_group_name = "${aws_subnet.reversal_public_webserver.id}"
-}
