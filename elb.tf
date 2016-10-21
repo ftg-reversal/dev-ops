0fb9ccea303e4d87a261eac7b868b43d06cd6761
tigberd <tigberd@gmail.com>
2016-10-22 05:22:35 +0900

    elbを作成

---
+++ elb.tf
@@ -0,0 +1,56 @@
+resource "aws_elb" "elb" {
+  name = "elb"
+  subnets = [
+    "${aws_subnet.reversal_public_webserver.id}"
+  ]
+  security_groups = [
+    "${aws_security_group.elb_security_group.id}",
+  ]
+
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 443
+    lb_protocol = "https"
+    ssl_certificate_id = "arn:aws:acm:ap-northeast-1:312746001499:certificate/f07612ab-6d60-4c4b-adc1-c51e132cdb18"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 5
+    target = "HTTP:80/"
+    interval = 30
+  }
+
+  instances = ["${aws_instance.reversal-webserver.id}"]
+  cross_zone_load_balancing = true
+  connection_draining = true
+  connection_draining_timeout = 400
+}
+
+resource "aws_security_group" "elb_security_group" {
+  name = "elb security group"
+  vpc_id = "${aws_vpc.reversal_vpc.id}"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
