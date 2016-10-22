8b0eb784db0e060072fef5c5a127ded608a530e4
tigberd <tigberd@gmail.com>
2016-10-22 14:57:26 +0900

    health check用にwebサーバの10080番を解放

--- ec2.tf
+++ ec2.tf
@@ -67,6 +67,13 @@ resource "aws_security_group" "webserver_security_group" {
   }
 
   ingress {
+    from_port       = 10080
+    to_port         = 10080
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
