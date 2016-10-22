2aec937d18ffb7602043ac3f3663f69716ff2ff8
tigberd <tigberd@gmail.com>
2016-10-22 13:57:26 +0900

    ELBが80番ポートからのアクセスを受け付けるように修正

--- elb.tf
+++ elb.tf
@@ -41,6 +41,13 @@ resource "aws_security_group" "elb_security_group" {
   vpc_id = "${aws_vpc.reversal_vpc.id}"
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
