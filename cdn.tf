26395816b0dbb90e1ff56a9597284257bc787d8f
tigberd <tigberd@gmail.com>
2017-02-22 21:40:30 +0900

    CDNのrestrictionsをnoneに修正

--- cdn.tf
+++ cdn.tf
@@ -39,8 +39,7 @@ resource "aws_cloudfront_distribution" "cdn" {
 
   restrictions {
     geo_restriction {
-      restriction_type = "whitelist"
-      locations        = ["US", "CA", "GB", "DE", "JP"]
+      restriction_type = "none"
     }
   }
 
